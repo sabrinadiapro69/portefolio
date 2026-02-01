052c00a9f63685d3e683020bad3bfffb84add41e
Nadia Bourouaha <sabrinadiapro69@gmail.com>
2026-02-01 09:36:51 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,22 @@
+async function loadQuote() {
+  const box = document.getElementById("quoteBox");
+
+  try {
+    const response = await fetch("https://api.quotable.io/random");
+
+    if (!response.ok) {
+      throw new Error("Erreur lors du chargement de la citation");
+    }
+
+    const data = await response.json();
+
+    box.innerHTML = `
+      <p>"${data.content}"</p>
+      <p><strong>— ${data.author}</strong></p>
+    `;
+  } catch (error) {
+    box.textContent = "Impossible de charger la citation.";
+  }
+}
+
 
